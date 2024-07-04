33c0f09367af897b5c2fea02b91df2a01cae0a02
BrettWilsonBDW <brett.david.wilson.bdw@outlook.com>
2024-07-04 13:51:24 +0200

    made preemptive initial table builder more robust

--- goal/preemptive.py
+++ goal/preemptive.py
@@ -13,15 +13,15 @@ def testInput():
     constraints = [
     ]
 
-    # goals = [
-    #     # <= is 0 and >= is 1
-    #     [12, 9, 15, 125, 1],
-    #     [5, 3, 4, 40, 2],
-    #     [5, 7, 8, 55, 0],
-    # ]
+    goals = [
+        # <= is 0 and >= is 1
+        [12, 9, 15, 125, 1],
+        [5, 3, 4, 40, 2],
+        [5, 7, 8, 55, 0],
+    ]
 
-    # constraints = [
-    # ]
+    constraints = [
+    ]
 
     # goals = [
     #     # <= is 0 and >= is 1 and == is 2
@@ -33,148 +33,343 @@ def testInput():
     # constraints = [
     # ]
 
-    goals = [
-        # <= is 0 and >= is 1 and == is 2
-        [7, 3, 40, 1],
-        [10, 5, 60, 1],
-        [5, 4, 35, 1],
-    ]
+    # goals = [
+    #     # <= is 0 and >= is 1 and == is 2
+    #     [7, 3, 40, 1],
+    #     [10, 5, 60, 1],
+    #     [5, 4, 35, 1],
+    # ]
 
-    constraints = [
-        [100, 60, 600, 0],
-    ]
+    # constraints = [
+    #     [100, 60, 600, 0],
+    # ]
+
+    # goals = [
+    #     # <= is 0 and >= is 1
+    #     [3, 1, 200, 2],
+    #     [300, 200, 20000, 0],
+    #     [150, 100, 11000, 1],
+    # ]
+
+    # constraints = [
+    # ]
 
     return goals, constraints
 
 
 def BuildFirstPreemptiveTableau(goalConstraints, constraints):
-    tabSizeH = len(constraints) + len(goalConstraints) * 2
-    tabSizeW = 1 + 1 + (len(goalConstraints) * 2) + \
-        len(constraints) + (len(goalConstraints[-1]) - 2)
+    # tabSizeH = len(constraints) + len(goalConstraints) * 2
+    # tabSizeW = 1 + 1 + (len(goalConstraints) * 2) + \
+    #     len(constraints) + (len(goalConstraints[-1]) - 2)
 
-    penlites = len(goalConstraints)
+    # penlites = len(goalConstraints)
+
+    # amtOfObjVars = (len(goalConstraints[-1]) - 2)
+
+    # tab = []
+
+    # tempGoalConstraints = []
+    # equalPositions = []
+    # for i in range(len(goalConstraints)):
+    #     if goalConstraints[i][-1] == 2:
+    #         tempGoalConstraints.append(goalConstraints[i])
+    #         equalPositions.append(i)
+    #         tabSizeH += 2
+    #         tabSizeW += 2
+    #         penlites += 1
+            
+    # tempGoals = copy.deepcopy(goalConstraints)
+
+    # fixedEqualGoals = []
+    # for lst in tempGoalConstraints:
+    #     # Copy the 1D list twice
+    #     listCopy1 = lst[:]
+    #     listCopy2 = lst[:]
+        
+    #     listCopy1[-1] = 0
+    #     listCopy2[-1] = 1
+        
+    #     fixedEqualGoals.append(listCopy1)
+    #     fixedEqualGoals.append(listCopy2)
+
+    # for i in range(len(equalPositions)):
+    #     del goalConstraints[equalPositions[i]]
+    #     goalConstraints.insert(equalPositions[i], fixedEqualGoals[i+1])
+    #     goalConstraints.insert(equalPositions[i], fixedEqualGoals[i])
+
+    # # initialize table with empty rows
+    # for i in range(tabSizeH):
+    #     tab.append([])
+
+    # # initialize table with zeros
+    # for i in range(tabSizeH):
+    #     for j in range(tabSizeW):
+    #         tab[i].append(0)
+
+    # # put in lhs in the z col
+    # for i in range(penlites):
+    #     tab[i][0] = 1
+
+    # # put in penlites
+    # gCtr = amtOfObjVars + 1
+    # for i in range(len(goalConstraints)):
+    #     if goalConstraints[i][-1] == 0:
+    #         tab[i][gCtr + 1] = -1
+    #     elif goalConstraints[i][-1] == 1:
+    #         tab[i][gCtr] = -1
+    #     gCtr += 2
+
+    # # put in goal constraints
+    # for i in range(penlites, tabSizeH - len(constraints)):
+    #     for j in range(1, amtOfObjVars + 1):
+    #         tab[i][-1] = goalConstraints[i - penlites][-2]
+    #         tab[i][j] = goalConstraints[i - penlites][j - 1]
+
+    # # TODO Test if this is working in equal constraints
+    # # put in normal constraints
+    # for i in range(tabSizeH - len(constraints), tabSizeH):
+    #     for j in range(1, (len(constraints[-1]) - 2) + 1):
+    #         tab[i][-1] = constraints[i - (tabSizeH - len(constraints))][-2]
+    #         tab[i][j] = constraints[i - (tabSizeH - len(constraints))][j - 1]
+
+
+    # # put in the slacks
+    # for i in range(len(constraints)):
+    #     if constraints[i][-1] == 0:
+    #         tab[tabSizeH - i - 1][-(i+2)] = 1
+    #     elif constraints[i][-1] == 1:
+    #         tab[tabSizeH - i - 1][-(i+2)] = -1
+
+    # # put the 1 -1 for goal constraints in
+    # onesCtr = amtOfObjVars + 1
+    # for i in range(penlites, tabSizeH - len(constraints)):
+    #     tab[i][onesCtr] = 1
+    #     tab[i][onesCtr + 1] = -1
+    #     onesCtr += 2
+
+    # # make new tab
+    # newTab = copy.deepcopy(tab)
+
+    # gCtr = len(goalConstraints)
+    # for i in range(penlites):
+    #     for j in range(len(tab[i])):
+    #         if goalConstraints[i][-1] == 0:
+    #             newTab[i][j] = tab[i + penlites][j] - tab[i][j]
+    #         elif goalConstraints[i][-1] == 1:
+    #             newTab[i][j] = tab[i + penlites][j] + tab[i][j]
+    #     gCtr += 1
+
+    # # fix the 2's aka the equalities
+    # gNegCtr = 0
+    # for i in range(len(tempGoals)):
+    #     if tempGoals[i][-1] == 2:
+    #         for k in range(len(newTab)):
+    #             del newTab[k][-(i + 3 + gNegCtr)]
+    #             del newTab[k][-(i + 2 + gNegCtr)]
+
+    #             del tab[k][-(i + 3 + gNegCtr)]
+    #             del tab[k][-(i + 2 + gNegCtr)]
+
+    #         newTab[i+1][-(i + 2 + gNegCtr)] = -1
+
+    #         tab[i+1][-(i + 3 + gNegCtr)] = -1
+
+    #         del newTab[-(i + 1)]
+    #         del tab[-(i + 1)]
+    #     gNegCtr += 1
+
+    # # for j in range(len(tab)):
+    # #     for k in range(len(tab[j])):
+    # #         print("{:10.3f}".format(tab[j][k]), end=" ")
+    # #     print()
+    # # print()
+    
+    # return tab, newTab, penlites
+
+    oldTab = []
+    newTab = []
+    conStart = 0
+
+
+    # print(penlites)
+    # print(goalConstraints)
+    # print(constraints)
+
+    # hight = (goals + goalConstraints) = goals * 2 + constraints
+    tabSizeH = (len(goalConstraints) * 2) + len(constraints)
+
+    # width = z + rhs + x + (g+,-) * 2 + slack
+    tabSizeW = 1 + 1 + (len(goalConstraints[-1]) - 2) + (len(goalConstraints) * 2) + len(constraints)
 
     amtOfObjVars = (len(goalConstraints[-1]) - 2)
-
-    tab = []
-
-    tempGoalConstraints = []
-    equalPositions = []
+    amtOfGoals = len(goalConstraints)
+    
+    # fix the equalities constraints sizes
     for i in range(len(goalConstraints)):
         if goalConstraints[i][-1] == 2:
-            tempGoalConstraints.append(goalConstraints[i])
-            equalPositions.append(i)
-            tabSizeH += 2
-            tabSizeW += 2
-            penlites += 1
-            
-    tempGoals = copy.deepcopy(goalConstraints)
+            tabSizeH += 1
+            amtOfGoals += 1
 
-    fixedEqualGoals = []
-    for lst in tempGoalConstraints:
-        # Copy the 1D list twice
-        listCopy1 = lst[:]
-        listCopy2 = lst[:]
-        
-        listCopy1[-1] = 0
-        listCopy2[-1] = 1
-        
-        fixedEqualGoals.append(listCopy1)
-        fixedEqualGoals.append(listCopy2)
+    # print(tabSizeH)
+    # print(tabSizeW)
 
-    for i in range(len(equalPositions)):
-        del goalConstraints[equalPositions[i]]
-        goalConstraints.insert(equalPositions[i], fixedEqualGoals[i+1])
-        goalConstraints.insert(equalPositions[i], fixedEqualGoals[i])
+
+
+    # print(amtOfObjVars)
+    # print(amtOfGoals)
 
     # initialize table with empty rows
     for i in range(tabSizeH):
-        tab.append([])
+        oldTab.append([])
 
     # initialize table with zeros
     for i in range(tabSizeH):
         for j in range(tabSizeW):
-            tab[i].append(0)
+            oldTab[i].append(0)
 
     # put in lhs in the z col
-    for i in range(penlites):
-        tab[i][0] = 1
+    for i in range(amtOfGoals):
+        oldTab[i][0] = 1
 
-    # put in penlites
+    # TODO keep an eye on the placement of the penalties for goal 2+ 2-
+    # put in penlites spots
     gCtr = amtOfObjVars + 1
+    ExtraCtr = 0
     for i in range(len(goalConstraints)):
         if goalConstraints[i][-1] == 0:
-            tab[i][gCtr + 1] = -1
+            oldTab[i + ExtraCtr][gCtr + 1] = -1
         elif goalConstraints[i][-1] == 1:
-            tab[i][gCtr] = -1
+            oldTab[i + ExtraCtr][gCtr] = -1
+        elif goalConstraints[i][-1] == 2:
+            oldTab[i][gCtr] = -1
+            oldTab[i + 1][gCtr + 1] = -1
+            ExtraCtr += 1
         gCtr += 2
 
-    # put in goal constraints
-    for i in range(penlites, tabSizeH - len(constraints)):
-        for j in range(1, amtOfObjVars + 1):
-            tab[i][-1] = goalConstraints[i - penlites][-2]
-            tab[i][j] = goalConstraints[i - penlites][j - 1]
-
-    # TODO Test if this is working in equal constraints
-    # put in normal constraints
-    for i in range(tabSizeH - len(constraints), tabSizeH):
-        for j in range(1, (len(constraints[-1]) - 2) + 1):
-            tab[i][-1] = constraints[i - (tabSizeH - len(constraints))][-2]
-            tab[i][j] = constraints[i - (tabSizeH - len(constraints))][j - 1]
-
+    # put in penlites values or leave as -1
+    # if len(penlites) != 0:
+    #     for i in range(len(oldTab)):
+    #         for j in range(len(oldTab[i])):
+    #             if oldTab[i][j] == -1:
+    #                 oldTab[i][j] = -penlites[i]
 
     # put in the slacks
     for i in range(len(constraints)):
         if constraints[i][-1] == 0:
-            tab[tabSizeH - i - 1][-(i+2)] = 1
+            oldTab[tabSizeH - i - 1][-(i+2)] = 1
         elif constraints[i][-1] == 1:
-            tab[tabSizeH - i - 1][-(i+2)] = -1
+            oldTab[tabSizeH - i - 1][-(i+2)] = -1
 
     # put the 1 -1 for goal constraints in
     onesCtr = amtOfObjVars + 1
-    for i in range(penlites, tabSizeH - len(constraints)):
-        tab[i][onesCtr] = 1
-        tab[i][onesCtr + 1] = -1
+    for i in range(amtOfGoals, tabSizeH - len(constraints)):
+        oldTab[i][onesCtr] = 1
+        oldTab[i][onesCtr + 1] = -1
         onesCtr += 2
 
-    # make new tab
-    newTab = copy.deepcopy(tab)
+    # put in goal constraints
+    for i in range(len(goalConstraints)):
+        for j in range(amtOfObjVars):
+            oldTab[i + amtOfGoals][j + 1] = goalConstraints[i][j] # lhs
+            oldTab[i + amtOfGoals][-1] = goalConstraints[i][-2] # rhs
 
-    gCtr = len(goalConstraints)
-    for i in range(penlites):
-        for j in range(len(tab[i])):
+    # put in constraints
+    for i in range(len(constraints)):
+        for j in range(amtOfObjVars):
+            oldTab[i + amtOfGoals + len(goalConstraints)][j + 1] = constraints[i][j] # lhs
+            oldTab[i + amtOfGoals + len(goalConstraints)][-1] = constraints[i][-2] # rhs
+
+
+    for j in range(len(oldTab)):
+        for k in range(len(oldTab[j])):
+            print("{:10.3f}".format(oldTab[j][k]), end=" ")
+        print()
+    print()
+
+
+    # first tab done move on to second tab
+    newTab = copy.deepcopy(oldTab)
+
+
+    # rows at the top of the old table are the goals
+    topRows = []
+    for i in range(amtOfGoals):
+        topRows.append([])
+        for j in range(len(oldTab[i])):
+            topRows[i].append(oldTab[i][j])
+
+    # print(topRows)
+
+    # rows at the bottom of the old table are the goal constraints
+    bottomRows = []
+    for i in range(len(goalConstraints)):
+        bottomRows.append([])
+        for j in range(len(oldTab[i])):
+            bottomRows[i].append(oldTab[i + amtOfGoals][j])
+
+    # print(bottomRows)
+
+    # fix the equalities issues by duplicating the goal constraint rows that belong to the equalities
+    tempBottomRows = []
+    for i in range(len(goalConstraints)):
+        if goalConstraints[i][-1] == 2:
+            listCopy = bottomRows[i][:]
+            
+            tempBottomRows.append(listCopy)
+            tempBottomRows.append(listCopy)
+        else:
+            tempBottomRows.append(bottomRows[i])
+
+    bottomRows = tempBottomRows
+
+    backUpGoals = copy.deepcopy(goalConstraints)
+    # fix the equalities issues by duplicating the goal constraint rows that belong to the equalities
+    tempGoals = []
+    for goal in goalConstraints:
+        if goal[-1] == 2:
+            listCopy1 = goal[:]
+            listCopy2 = goal[:]
+            
+            listCopy1[-1] = 0
+            listCopy2[-1] = 1
+            
+            tempGoals.append(listCopy2)
+            tempGoals.append(listCopy1)
+        else:
+            tempGoals.append(goal)
+
+    goalConstraints = tempGoals
+
+    # keep in mind this is penalties initial tab calculations
+
+    # calculate the new table goal rows
+    for i in range(len(goalConstraints)):
+        for j in range(len(newTab[i])):
             if goalConstraints[i][-1] == 0:
-                newTab[i][j] = tab[i + penlites][j] - tab[i][j]
+                # newTab[i][j] = -1 * (topRows[i][j] - (bottomRows[i][j] * penlites[i]))
+                newTab[i][j] = -1 * (topRows[i][j] - (bottomRows[i][j]))
             elif goalConstraints[i][-1] == 1:
-                newTab[i][j] = tab[i + penlites][j] + tab[i][j]
-        gCtr += 1
+                newTab[i][j] = (topRows[i][j] + (bottomRows[i][j]))
 
-    # fix the 2's aka the equalities
-    gNegCtr = 0
-    for i in range(len(tempGoals)):
-        if tempGoals[i][-1] == 2:
-            for k in range(len(newTab)):
-                del newTab[k][-(i + 3 + gNegCtr)]
-                del newTab[k][-(i + 2 + gNegCtr)]
+    # fix the order of the equalities
+    equalCtr = 0
+    for i in range(len(backUpGoals)):
+        if backUpGoals[i][-1] == 2:
+            tempRow = newTab[i + equalCtr]
+            newTab[i + equalCtr] = newTab[i + 1 + equalCtr]
+            newTab[i + 1 + equalCtr] = tempRow
 
-                del tab[k][-(i + 3 + gNegCtr)]
-                del tab[k][-(i + 2 + gNegCtr)]
+            equalCtr += 1
+            
+    print()
+    for j in range(len(newTab)):
+        for k in range(len(newTab[j])):
+            print("{:10.3f}".format(newTab[j][k]), end=" ")
+        print()
+    print()
 
-            newTab[i+1][-(i + 2 + gNegCtr)] = -1
-
-            tab[i+1][-(i + 3 + gNegCtr)] = -1
-
-            del newTab[-(i + 1)]
-            del tab[-(i + 1)]
-        gNegCtr += 1
-
-    # for j in range(len(tab)):
-    #     for k in range(len(tab[j])):
-    #         print("{:10.3f}".format(tab[j][k]), end=" ")
-    #     print()
-    # print()
-    
-    return tab, newTab, penlites
+    conStart = amtOfGoals
+    return oldTab, newTab, conStart
 
 
 def DoPivotOperations(tab, conStartRow, zRow, tabNum=1):
